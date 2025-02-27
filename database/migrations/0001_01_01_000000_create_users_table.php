c13eacc4083005ad075e55a351c9581697720ef9
rivalramadhan <ripallez@gmail.com>
2025-02-27 21:50:03 +0700

    feat: add unique nik and nisn fields to users table for enhanced user identification

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('nik')->unique();
+            $table->string('nisn')->unique();
             $table->string('fullname');
             $table->string('email');
             $table->timestamp('phone')->nullable();
