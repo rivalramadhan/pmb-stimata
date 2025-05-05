64aab02b0503e925a4b12e9255e64af85ed843c7
rivalramadhan <ripallez@gmail.com>
2025-05-05 15:57:20 +0700

    feat: add DocumentController and PaymentController with resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+
 Route::resource('users', UserController::class);
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('payments', PaymentController::class);
+Route::resource('documents', DocumentController::class);
