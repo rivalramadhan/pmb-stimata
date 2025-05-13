63b7a4a73b4c018b3908033250364aa639dd2cdd
rivalramadhan <ripallez@gmail.com>
2025-05-13 23:57:51 +0700

    feat: update PaymentController to retrieve all payments for management view and enhance store method validation

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Payment;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
     public function index()
     {
-        return view('Payment.ManagePayment');
+        $payments = Payment::all();
+        return view('Payment.ManagePayment', compact('payments'));
     }
 
     public function create()
@@ -19,12 +22,19 @@ class PaymentController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'amount' => 'required|string|min:0',
-            'payment_method' => 'required|string|max:255',
-            'description' => 'nullable|string|max:255',
+            'fullname' => 'required|string|max:255',
+            'amount' => 'required|string|max:255',
             'status' => 'required|string|in:pending,completed,cancelled',
             'notes' => 'nullable|string|max:255',
             'image_path' => 'required|string|max:255',
+        ]);
+
+        User::create([
+            'fullname' => $request->fullname,
+            'amount' => $request->amount,
+            'status' => $request->status,
+            'notes' => $request->notes,
+            'image_path' => $request->image_path,
         ]);
 
         return view('payment.ManagePayment')->with('success', 'Payment created successfully.');
